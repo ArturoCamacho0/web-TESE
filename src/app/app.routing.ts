86935d84a62a5963953201fa4b5fd6f4b2e2a7eb
Pepe el toro <programacionandmore@gmail.com>
2021-06-07 14:11:46 -0500

    Apartado de contacto

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,12 @@
 import { ModuleWithProviders } from '@angular/core';
 import {Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/sections/home/home.component';
+import { ContactoComponent } from './components/sections/contacto/contacto.component';
+
 // Rutas
 const appRoutes: Routes = [
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent },
+    { path: 'contacto', component: ContactoComponent }
 ];
 
 // Exportar el modulo
--- src/app/components/sections/sections.module.ts
+++ src/app/components/sections/sections.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { HomeComponent } from './home/home.component';
 import { MaterialModule } from '../material/material.module';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    ContactoComponent
   ],
   imports: [
     CommonModule,
