8dc7f5de413521b3c3129b266e9aec873cd5826f
SYSTEM-OTMX <gtainside222@gmail.com>
2021-06-10 23:48:24 -0500

    COMMIT MASTER CON COMPONNENTES

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,19 @@
 import { ModuleWithProviders } from '@angular/core';
 import {Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/sections/home/home.component';
+import { IdentidadInstitucionalComponent } from './components/sections/identidad-institucional/identidad-institucional.component';
+import { HistoriaComponent } from './components/sections/identidad-institucional/Historia/Historia.component';
+import { LogoComponent } from './components/sections/identidad-institucional/Logo/Logo.component';
+import { MisionVisionComponent } from './components/sections/identidad-institucional/mision-vision/mision-vision.component';
+import { OrganigramaComponent } from './components/sections/identidad-institucional/Organigrama/Organigrama.component';
 // Rutas
 const appRoutes: Routes = [
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent },
+    { path: 'Identidad', component: IdentidadInstitucionalComponent },
+    { path: 'Historia', component: HistoriaComponent },
+    { path: 'Logo', component: LogoComponent },
+    { path: 'Mision_Vision', component: MisionVisionComponent },
+    { path: 'Organigrama', component: OrganigramaComponent },
 ];
 
 // Exportar el modulo
