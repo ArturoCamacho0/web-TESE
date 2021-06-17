90bf76c24d5a98a70f0da34c03c7fc5449f534be
Fosrey <fosrey@gmail.com>
2021-06-16 22:41:20 -0500

    Pagina de Inscripcion

---
+++ src/app/components/sections/admisionytramites/inscripcion/inscripcion.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InscripcionComponent } from './inscripcion.component';
+
+describe('InscripcionComponent', () => {
+  let component: InscripcionComponent;
+  let fixture: ComponentFixture<InscripcionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ InscripcionComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(InscripcionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/components/sections/sections.module.ts
+++ src/app/components/sections/sections.module.ts
@@ -6,13 +6,17 @@ import { MaterialModule } from '../material/material.module';
 import { BannerPComponent } from './banner-p/banner-p.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { PlantillaContenidoComponent } from './plantilla-contenido/plantilla-contenido.component';
+import { AdmisionytramitesComponent } from './admisionytramites/admisionytramites.component';
+import { InscripcionComponent } from './admisionytramites/inscripcion/inscripcion.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     BannerPComponent,
     CarouselComponent,
-    PlantillaContenidoComponent
+    PlantillaContenidoComponent,
+    AdmisionytramitesComponent,
+    InscripcionComponent
   ],
   imports: [
     CommonModule,
