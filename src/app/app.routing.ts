33e498d81add751fc016cba09f089553f8c76c58
SYSTEM-OTMX <gtainside222@gmail.com>
2021-06-19 00:30:16 -0500

    plantilla a usar, final

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,18 +1,15 @@
 import { ModuleWithProviders } from '@angular/core';
 import {Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/sections/home/home.component';
-
-//import carreras
-import { CuerpoComponent } from './components/sections/carrera/cuerpo/cuerpo.component';
-import { CuerpoSistemasComponent } from './components/sections/carrera/profesion/SistemasComputacionales/cuerpo-sistemas/cuerpo-sistemas.component';
-
-
+import { PlantillaContenidoComponent } from './components/sections/plantilla-contenido/plantilla-contenido.component';
+import {AdmisionytramitesComponent} from './components/sections/admisionytramites/admisionytramites.component';
+import { InscripcionComponent } from './components/sections/admisionytramites/inscripcion/inscripcion.component';
 // Rutas
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'Carreras', component: CuerpoComponent},
-    { path:'Carreras/Sistemas', component: CuerpoSistemasComponent}
-
+    { path: 'plantillacontenido', component:PlantillaContenidoComponent },
+{path: 'inscripcion', component:InscripcionComponent },
+{path:'admisionytramites',component:AdmisionytramitesComponent}
 ];
 
 // Exportar el modulo
---
+++ src/app/components/sections/admisionytramites/inscripcion/inscripcion.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { CAROUSEL_DATA_ITEMS_BANNERP } from './carousel';
+import { ICarouselItem } from '../../carousel/icarousel-item.metadata';
+@Component({
+  selector: 'app-inscripcion',
+  templateUrl: './inscripcion.component.html',
+  styleUrls: ['./inscripcion.component.css']
+})
+export class InscripcionComponent implements OnInit {
+  public carouselData: ICarouselItem[]=CAROUSEL_DATA_ITEMS_BANNERP;
+  constructor() { }
+
+
+  ngOnInit(): void {
+  }
+
+  onClick(){
+    let full = document.getElementById('side');
+    full.classList.toggle('active');
+  }
+
+  open(){
+    let r = document.getElementById('right');
+    r.classList.toggle('open');
+
+    let l = document.getElementById('left');
+    l.classList.toggle('open');
+  }
+
+
+}
