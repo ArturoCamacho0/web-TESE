5b5780fd90b75ecfba5ccd33915d082edc85b0d3
Arturo Camacho <programacionandmore@gmail.com>
2021-06-20 22:24:08 -0500

    Convocatorias

--- src/app/components/sections/home/carousel.ts
+++ src/app/components/sections/home/carousel.ts
@@ -3,29 +3,80 @@ export const CAROUSEL_DATA_ITEMS_BANNERP: ICarouselItem[] = [
     {
         id: 1,
         title: {
-            first: 'Sobre nosotros',
-            second: '1'
+            first: 'POSIBLE 2021',
+            second: 'Convocatoria'
         },
         subtitle: 'Prueba de imágen',
-        link: 'localhost:4200/asd',
-        image: "assets/images/tese1.jpg"
+        link: 'http://www.tese.edu.mx/documentos2004/10271_ZDOCQBY.pdf',
+        image: "assets/images/convocatorias/posible.png"
     }, {
         id: 2,
         title: {
-            first: 'Noticias',
-            second: ''
+            first: 'Pagos',
+            second: 'Aviso'
         },
         subtitle: '',
-        link: 'Carreras/Industrial',
-        image: "assets/images/tese2.jpg"
+        link: 'assets/images/convocatorias/aviso_pagos.jpg',
+        image: "assets/images/convocatorias/aviso_pagos.jpg"
     }, {
         id: 3,
         title: {
-            first: 'Anuncios',
-            second: ''
+            first: 'Maestría en ingeniería en sistemas computacionales',
+            second: 'Convocatoria'
         },
         subtitle: '',
-        link: 'Carreras/Industrial',
-        image: "assets/images/tese3.jpg"
-    }
+        link: 'http://www.tese.edu.mx/documentos2004/9267_RHCRZGM.pdf',
+        image: "assets/images/convocatorias/maestrias.png"
+    },
+    {
+        id: 4,
+        title: {
+            first: 'Maestría en Eficiencia Energética y Energías Renovables',
+            second: 'Convocatoria'
+        },
+        subtitle: '',
+        link: 'http://www.tese.edu.mx/documentos2004/9231_ECZKSEI.pdf',
+        image: "assets/images/convocatorias/energias_r.png"
+    },
+    {
+        id: 5,
+        title: {
+            first: '6° Premio Raquel Berman Frente a Adversidad',
+            second: 'Convocatoria'
+        },
+        subtitle: '',
+        link: 'http://www.tese.edu.mx/documentos2004/10266_STIRLHF.pdf',
+        image: "assets/images/convocatorias/raquel_berman.jpg"
+    },
+    {
+        id: 6,
+        title: {
+            first: 'Maestria en Ciencias en Ingeniería Bioquimica 2021-2',
+            second: 'Convocatoria'
+        },
+        subtitle: '',
+        link: 'http://www.tese.edu.mx/documentos2004/9258_UCGNRSN.pdf',
+        image: "assets/images/convocatorias/bioquimica.jpg"
+    },
+    {
+        id: 7,
+        title: {
+            first: 'Programa Escuelas por la Tierra 2021',
+            second: 'Convocatoria'
+        },
+        subtitle: '',
+        link: 'http://www.tese.edu.mx/documentos2004/9182_YVWCKKR.pdf',
+        image: "assets/images/convocatorias/tierra.jpg"
+    },
+    {
+        id: 8,
+        title: {
+            first: 'Maestría en Gestión Administración 2021-2',
+            second: 'Convocatoria'
+        },
+        subtitle: '',
+        link: 'http://www.tese.edu.mx/documentos2004/9184_VBSCKOC.pdf',
+        image: "assets/images/convocatorias/maestrias.png"
+    },
+    
 ]
