4387175458f2475f1a2b87639181294b74ccb42e
SYSTEM-OTMX <gtainside222@gmail.com>
2021-07-21 21:01:07 -0500

    VERSION FINAL 1.1

--- src/app/components/sections/sections.module.ts
+++ src/app/components/sections/sections.module.ts
@@ -60,6 +60,14 @@ import { CuerpoMecatronicaComponent } from './carrera/profesion/IngenieriaMecatr
 import { CarouselMecatronicaComponent } from './carrera/profesion/IngenieriaMecatronica/carousel-mecatronica/carousel-mecatronica.component';
 import { CarouselQuimicaComponent } from './carrera/profesion/IngenieriaQuimica/carousel-quimica/carousel-quimica.component';
 import { CuerpoQuimicaComponent } from './carrera/profesion/IngenieriaQuimica/cuerpo-quimica/cuerpo-quimica.component';
+
+
+
+
+
+
+
+/**IDENTIDAD INSTITUCIONNAL */
 import { AdministracionLogoComponent } from './Identidad-institucional/Administracion/administracion-logo/administracion-logo.component';
 import { AdministracionHistoriaComponent } from './Identidad-institucional/Administracion/administracion-historia/administracion-historia.component';
 import { AdministracionMisionComponent } from './Identidad-institucional/Administracion/administracion-mision/administracion-mision.component';
